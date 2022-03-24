656b98285c540b0f1bab2069359021b03f0ed168
Adramaryl <alice.lacroix.95@gmail.com>
2022-03-24 18:43:08 +0100

    display article list

---
+++ articles/index.php
@@ -0,0 +1,10 @@
+<?php
+
+require "model.php";
+
+$articles_list = getMappedArticles();
+
+require "view.php";
+
+
+?>
---
+++ index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+    <?php $title = "Accueil"; require "head.php"; ?>
+    <body>
+        <?php require "header.php"; ?>
+        <?php require "footer.php"; ?>
+    </body>
+</html>
