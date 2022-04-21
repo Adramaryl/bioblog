082bed2e64842699c3176d8b6a00b8ca8ea09264
Adramaryl <alice.lacroix.95@gmail.com>
2022-04-21 21:34:32 +0200

    add pagination to articles

--- articles/index.php
+++ articles/index.php
@@ -1,8 +1,16 @@
 <?php
 
+define('ROW_PER_PAGE', 3);
+
 require "model.php";
 
-$articles_list = getMappedArticles();
+$page = 1;
+
+if (!empty($_GET['page'])) {
+    $page = (int)$_GET['page'];
+}
+
+$articles_list = getMappedArticles($page);
 
 require "view.php";
 
