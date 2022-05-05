1c717ed17a63b85ca0bfe4d4d0b6335d8e03dd30
Adramaryl <alice.lacroix.95@gmail.com>
2022-05-05 21:34:35 +0200

    add login link

--- header.php
+++ header.php
@@ -1,5 +1,6 @@
 <header>
     <nav class="navbar navbar-light bg-light">
-        <h1 class="navbar-brand mb-0"><?= $appName ?></h1>
+        <h1 class="navbar-brand mb-0 mr-auto"><a href="/bio_blog"><?= $appName ?></a></h1>
+        <a href="/bio_blog/admin" class="btn btn-warning">Admin</a>
     </nav>
 </header>
