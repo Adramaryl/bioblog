329176d2837c137d536b30d9803ee7b5a913ddee
Adramaryl <alice.lacroix.95@gmail.com>
2022-05-05 21:22:30 +0200

    add login and logout

--- login/index.php
+++ login/index.php
@@ -5,22 +5,37 @@ require './model.php';
 require_once '../helpers/form-helper.php';
 require_once '../helpers/auth-helper.php';
 
+$hasError = false;
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $user_infos = [
         'username' => sanitize_input($_POST['username']),
         'password' => sanitize_input($_POST['password'])
     ];
 
-    $user = 'bob';
-    $pass = password_hash('bob', PASSWORD_DEFAULT);
+    $user = checkUser($user_infos['username'], $user_infos['password']);
 
-    if ($user_infos['username'] === $user && password_verify($user_infos['password'], $pass)) {
+    if (isset($user)) {
         init_session();
-        $_SESSION['login'] = $user_infos['username'];
-        
+        $_SESSION['login'] = $user;
+
         $redirect = isset($_GET['redirect']) ? urldecode($_GET['redirect']) : '../articles' ;
         redirect($redirect);
+    } else {
+        $hasError = true;
     }
+
+    //$user = 'bob';
+    //$pass = password_hash('bob', PASSWORD_DEFAULT);
+
+    //if ($user_infos['username'] === $user && password_verify($user_infos['password'], $pass)) {
+    //    init_session();
+    //    $_SESSION['login'] = $user_infos['username'];
+    //    
+    //    $redirect = isset($_GET['redirect']) ? urldecode($_GET['redirect']) : '../articles' ;
+    //    redirect($redirect);
+    //}
+
 }
 
 require './view.php';
