01398a476faccd0f5c22320ba660946b346b3179
Adramaryl <alice.lacroix.95@gmail.com>
2022-05-12 21:34:49 +0200

    block access to create articles

--- create_article/index.php
+++ create_article/index.php
@@ -4,6 +4,8 @@ require "model.php";
 require "../helpers/form-helper.php";
 require "../helpers/auth-helper.php";
 
+prevent_not_connected(true);
+
 function validateInputs($inputs) {
     $errors = [];
     if (empty(trim($inputs['title']))) {
